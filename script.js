87d227a74a8e06c5e33994f75156c5fb4a8da597
princegupta-in <zprincegupta@gmail.com>
2024-07-27 21:01:05 +0530

    update

--- script.js
+++ script.js
@@ -9,32 +9,31 @@ function formatTime(seconds) {
 async function getSongs() {
     const data = await fetch(url);
     const texts = await data.text();
-    // console.log(texts);
     let tempDiv = document.createElement("div");
-    tempDiv.innerHTML = texts;//document.createElement("div").innerHTML = texts;
-    // console.log(div)
+    tempDiv.innerHTML = texts;
     let as = tempDiv.getElementsByTagName("a");
-    // console.log(as);
     const songs = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.replaceAll("%20", " "))
+            songs.push(element.href.replaceAll("%20", " "));
         }
     }
     return songs;
 }
 
-
 // this will ensure that only one song play at a time
-let audio = null; // Declare the audio variable globally
-function playIt(songUrl, songTitle) {
+// Declare the audio variable globally
+let audio = null;
+let currentSongIndex = 0; // Global variable to keep track of the current song index
+let songs = []; // Global variable to store the list of songs
+
+function playIt(songUrl, songTitle, index) {
     // Stop any currently playing audio
     if (audio) {
         audio.pause();
-        // play_me.src="play.svg";
     }
-    // Play the new audio
+    // then play the new audio
     audio = new Audio(songUrl);
     audio.play();
     play_me.src = "pause.svg";
@@ -42,79 +41,98 @@ function playIt(songUrl, songTitle) {
     //in playBar display song name and time
     document.querySelector(".songInfo").innerHTML = songTitle;
 
-    //getting the audio time and duration
+    //currentTime and duration are methods
     audio.addEventListener("timeupdate", () => {
-        // console.log(audio.currentTime, audio.duration);
         let currentTime = audio.currentTime;
         let duration = audio.duration;
-        document.querySelector(".songTime").innerHTML=`${formatTime(currentTime)}/${formatTime(duration)}`
+        document.querySelector(".songTime").innerHTML = `${formatTime(currentTime)}/${formatTime(duration)}`;
+        document.querySelector(".circle").style.left = `${(currentTime / duration) * 100}%`;
+    });
 
-    })
+    document.querySelector(".seekBar").addEventListener("click", (e) => {
+        document.querySelector(".circle").style.left = ((e.offsetX / e.target.getBoundingClientRect().width) * 100) + "%";
+        audio.currentTime = ((e.offsetX / e.target.getBoundingClientRect().width) * audio.duration);
+    });
+
+    // Change play button back to play icon when audio ends
+    audio.addEventListener("ended", () => {
+        document.querySelector("#play_me").src = "play.svg";
+    });
+
+    currentSongIndex = index; // Update the current song index
 }
 
-
-
-// console.log(getSongs());
 async function main() {
-    const songs = await getSongs();
-    console.log(songs);
-
-
+    songs = await getSongs(); // Assign songs to the global variable
     const songList = document.querySelector(".songList");
-    // songList.innerHTML = "";
 
-    songs.forEach(element => {
-        console.log(element.split("/songs/")[1]);
+    songs.forEach((element, index) => {
         const songTitle = element.split("/songs/")[1];
         const listItem = document.createElement("li");
-        // listItem.textContent = songTitle;
         songList.appendChild(listItem);
-        listItem.innerHTML = `<div class="miniBar ">
-                            <div class="music-logo"><img class="invert" src="music.svg" alt=""></div>
-                            <div class="info">
-                                <div class="song-Name">${songTitle}</div>
-                                <div class="artist-Name">artist name</div>
-                            </div>
-                            <div class="play-option">
-                                <span>play now</span>
-                                <img class="invert" src="play.svg" alt="">
-                            </div>`
+        listItem.innerHTML = `<div class="miniBar">
+                                <div class="music-logo"><img class="invert" src="music.svg" alt=""></div>
+                                <div class="info">
+                                    <div class="song-Name">${songTitle}</div>
+                                    <div class="artist-Name">artist name</div>
+                                </div>
+                                <div class="play-option">
+                                    <span>play now</span>
+                                    <img class="invert" src="play.svg" alt="">
+                                </div>`;
 
 
         //adding event listener to the play now button
         //will call the playIt function created in global
-        //as element is accessible here in this block only thats why i am doing this stuff here, note that here element is already object of arrar, hopefully i have used the correct word here "object".   
+        //as element is accessible here in this block only thats why i am doing this stuff here, note that here element is already object of array, hopefully i have used the correct word here "object".                        
         listItem.querySelector(".play-option").addEventListener("click", () => {
-            playIt(element, songTitle);
-        })
-
-
-    })
+            playIt(element, songTitle, index);
+        });
+    });
     //if play then pause and viseversa
     document.querySelector(".songButton").querySelector("#play_me").addEventListener("click", () => {
         if (audio) {
             if (audio.paused) {
-                audio.play()
-                play_me.src = "pause.svg"
-            }
-            else {
-                audio.pause()
-                play_me.src = "play.svg"
+                audio.play();
+                play_me.src = "pause.svg";
+            } else {
+                audio.pause();
+                play_me.src = "play.svg";
             }
         }
+    });
+
+    document.querySelector("#hamburger").addEventListener("click", () => {
+        document.querySelector(".left").style.left = 0 + `%`;
+    });
+
+    document.querySelector(".cutIcon").addEventListener("click", () => {
+        document.querySelector(".left").style.left = -100 + `%`;
+    });
+
+    document.querySelector("#next-me").addEventListener("click", () => {
+        if (currentSongIndex < songs.length - 1) {
+            currentSongIndex++;
+        } else {
+            currentSongIndex = 0; // Loop back to the first song if at the end of the list
+        }
+        const nextSongUrl = songs[currentSongIndex];
+        const nextSongTitle = nextSongUrl.split("/songs/")[1];
+        playIt(nextSongUrl, nextSongTitle, currentSongIndex);
+    });
+
+    document.querySelector("#previous-me").addEventListener("click", () => {
+        if (currentSongIndex > 0) {
+            currentSongIndex--;
+        } else {
+            currentSongIndex = (songs.length - 1);
+        }
+        const previousSongUrl = songs[currentSongIndex];
+        const previousSongTitle = previousSongUrl.split("/songs/")[1];
+        playIt(previousSongUrl, previousSongTitle, currentSongIndex);
     })
-
-
-
-    // audio.addEventListener("timeupdate", () => {
-    //     const currentTime = formatTime(audio.currentTime);
-    //     console.log(currentTime);
-
-    // })
 }
 
 
-
-main()
-
+main();
 
